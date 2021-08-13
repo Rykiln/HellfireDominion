d926a93aa9aa70c9eeb7c1a7ea2b82412d827030
Rykiln <41490808+Rykiln@users.noreply.github.com>
2021-08-13 14:46:40 -0500

    Update tag.js
    Fixed Descriptions in Comments.
    Converted multiple addField objects into an addField object array.

--- commands/utility/tag.js
+++ commands/utility/tag.js
@@ -1,17 +1,17 @@
 const Discord = require("discord.js");
 module.exports = {
 	name: 'tag',													// Name of this command. Required for all commands.
-	description: 'Allow players to apply to open cores.',							// [Optional] Description of this command for the help command
+	description: 'Allow players to apply to open cores.',			// [Optional] Description of this command for the help command
 	aliases: [`farmtag`, `apply`],						 			// [Optional] Permits additional command names to be used for this command 
 	usage: '<CharacterName> <Trial Role> <URL To ESO Logs>',		// [Optional] Displays how to use this command in the help command.
-    // permissions: `MANAGE_ROLES`,									// [Optional] Checks for default discord.js permissions. See https://discord.js.org/#/docs/main/stable/class/Permissions?scrollTo=s-FLAGS
-    args: true, 													// [Optional] When True - Requires Arguments Be Provided In Message Object
+	// permissions: `MANAGE_ROLES`,									// [Optional] Checks for default discord.js permissions. See https://discord.js.org/#/docs/main/stable/class/Permissions?scrollTo=s-FLAGS
+	args: true, 													// [Optional] When True - Requires Arguments Be Provided In Message Object
 	guildOnly: true, 												// [Optional] When True - Prevents Command from being used in a Direct Message With The Bot Account
 	cooldown: 5, 													// [Optional] See https://discordjs.guide/command-handling/adding-features.html#cooldowns
 	execute(msgObject, args, client) {
-		const channelLogs = msgObject.guild.channels.cache.get(process.env.HD_CHANNEL_TRIALTAGRESULTS) // #trial-tag-results channel
+		const channelLogs = msgObject.guild.channels.cache.get(process.env.HD_CHANNEL_TRIALTAGRESULTS) // #open-cores-application-feedback channel
 		const values = args.join(` `).split(/[,]/);
-		
+
 		const displayEmptyMessage = "None Provided";
 		const requestCharacterName = values[0] || displayEmptyMessage;
 		const requestCharacterRole = values[1] || displayEmptyMessage;
@@ -24,11 +24,13 @@ module.exports = {
 			.setThumbnail(`https://images.uesp.net/thumb/7/7e/ON-icon-heraldry-Deities_Akatosh.png/100px-ON-icon-heraldry-Deities_Akatosh.png`)
 			.setFooter(client.user.username, client.user.displayAvatarURL())
 			.setTimestamp()
-			.addField(`Applicant`, msgObject.author)
-			.addField(`Character Name`, requestCharacterName)
-			.addField(`Trial Role`, requestCharacterRole)
-			.addField(`Trial Logs`, requestCharacterLog)
-			.addField(`Comments`, requestComments);
+			.addFields(
+				{ name: `Applicant`, value: msgObject.author },
+				{ name: `Character Name`, value: requestCharacterName },
+				{ name: `Trial Role`, value: requestCharacterRole },
+				{ name: `Trial Logs`, value: requestCharacterLog },
+				{ name: `Comments`, value: requestComments }
+			)
 		msgObject.reply(`Thank you ${msgObject.author}! Your request has been submitted for review!`);
 		channelLogs.send(embed)
 			.then(e => e.react(`✅`))
