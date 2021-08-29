13a375a31fbbc613756b6a90fca7bfc513060e69
Lucas Koontz <lucas.koontz@wildlifestudios.com>
2021-08-29 11:04:57 -0300

    Add verification system

--- Commands/Admin/updateprerequisites.js
+++ Commands/Admin/updateprerequisites.js
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require("discord.js");
-const { readFile } = require(`fs`);
+const {promises: {readFile}} = require("fs");
 
 module.exports = {
 	// Name of this command. Required for all commands.
@@ -25,41 +25,144 @@ module.exports = {
 
 	// [Optional] See https://discordjs.guide/command-handling/adding-features.html#cooldowns
 	cooldown: 5,
-	execute(msgObject) {
+	execute(msgObject, _, client) {
+		const fileVerificationSystem = `./Data/HellfireDominion/verification_system.json`;
 		const filePrerequisites = `./Data/HellfireDominion/prerequisites.json`;
+		const filePrerequisitesAdditionalGear = `./Data/HellfireDominion/prerequisites_additional_gear.json`;
+		const fileRanks = `./Data/HellfireDominion/ranks.json`;
+		const fileMythicRank = `./Data/HellfireDominion/ranks_mythic.json`;
 
-		readFile(filePrerequisites, function (err, data) {
-			if (err) throw err;
-			const prerequisites = JSON.parse(data);
-			function getRoleData(JSONObject, value) {
-				// Get Object Keys
-				const keys = Object.keys(JSONObject);
-				keys.forEach(key => {
-					const subkeys = JSONObject[key];
-					const role = msgObject.guild.roles.resolve(subkeys.roleID);
-					const ddID = msgObject.guild.roles.resolve(subkeys.ddID);
-					const healerID = msgObject.guild.roles.resolve(subkeys.healerID);
-					const tankID = msgObject.guild.roles.resolve(subkeys.tankID);
-					const dpsMag = `${subkeys.dpsMag}K for Magicka`;
-					const dpsStam = `${subkeys.dpsStam}K for Stamina`;
-					const healerGear = Object.keys(subkeys.gearHealer).map(setName => `[${subkeys.gearHealer[setName].setName}](${subkeys.gearHealer[setName].setURL})`);
-					const tankGear = Object.keys(subkeys.gearTank).map(setName => `[${subkeys.gearTank[setName].setName}](${subkeys.gearTank[setName].setURL})`);
-					const restrictions = [
-						`No Mythic Items`, `No [Blood for Blood](https://eso-hub.com/en/skills/world/vampire/blood-for-blood)`, `No [Blood Frenzy](https://eso-hub.com/en/skills/world/vampire/blood-frenzy)`]
-					const embed = new MessageEmbed()
-						.setTitle(role.name)
-						.setColor(role.color)
-						.addFields(
-							{ name: `DPS`, value: `${ddID}\n\n${dpsMag}\n${dpsStam}\n${restrictions.join(`\n`)}`, inline: true },
-							{ name: `Healer`, value: `${healerID}\n\n${healerGear.join(`\n`)}`, inline: true },
-							{ name: `Tank`, value: `${tankID}\n\n${tankGear.join(`\n`)}`, inline: true }
-						)
-					msgObject.channel.send({ embeds: [embed] })
-				});
+		// Get Guild Information From JSON File
+		//const guildHellfireDominion = `./Data/hd.json`;
 
+		const guildIcon = client.user.displayAvatarURL();
+
+		// Filler to correct embed width. Each embed has 36 spacings.
+		const spacing = `\b　`;
+		const embedWidth = 36;
+
+
+		function sendEmbed(title, description, color, fields = null, thumbnail = false, timestamp = false) {
+			let embed = new MessageEmbed()
+				.setTitle(title)
+				.setDescription(description)
+				.setColor(color)
+				.setFooter(client.user.username, client.user.displayAvatarURL())
+
+			if (fields) {
+				embed.setFields(fields);
 			}
-			getRoleData(prerequisites, `fine`)
-		});
+			if (thumbnail) {
+				embed.setThumbnail(guildIcon)
+			}
+			if(timestamp) {
+				embed.setTimestamp()
+			}
 
+			msgObject.channel.send({embeds: [ embed ]});
+		}
+
+		Promise.all([
+			readFile(fileVerificationSystem),
+			readFile(filePrerequisites),
+			readFile(filePrerequisitesAdditionalGear),
+			readFile(fileRanks),
+			readFile(fileMythicRank),
+		]).then(([verificationSystemData, prerequisitesData, prerequisitesAdditionalData, ranksData, rankMythicData], ) => {
+			const verficationJSONObject = JSON.parse(verificationSystemData);
+
+			// General Verification System Information
+			const verificationSystem = verficationJSONObject["verificationSystem"];
+			sendEmbed(verificationSystem.title, verificationSystem.description.join('\n'), process.env.HD_COLOR_ORANGE, false, true);
+
+			// Prerequisites Information
+			const prerequisitesSystem = verficationJSONObject["prerequisites"];
+			sendEmbed(prerequisitesSystem.title, prerequisitesSystem.description.join('\n'), process.env.HD_COLOR_ORANGE);
+
+			
+			// Get Prerequisites
+			const prerequisitesJSONObject = JSON.parse(prerequisitesData);
+			const prerequisiteKeys = Object.keys(prerequisitesJSONObject);
+			const prerequisiteAdditionalJSONObject = JSON.parse(prerequisitesAdditionalData);
+			
+			prerequisiteKeys.forEach(key => {
+				const subkeys = prerequisitesJSONObject[key];
+				const role = msgObject.guild.roles.resolve(subkeys.roleID);
+				const ddID = msgObject.guild.roles.resolve(subkeys.ddID);
+				const healerID = msgObject.guild.roles.resolve(subkeys.healerID);
+				const tankID = msgObject.guild.roles.resolve(subkeys.tankID);
+				const dpsMag = `${subkeys.dpsMag}K for Magicka`;
+				const dpsStam = `${subkeys.dpsStam}K for Stamina`;
+				const healerGear = Object.keys(subkeys.gearHealer)
+																 .map(setName => `[${subkeys.gearHealer[setName].setName}](${subkeys.gearHealer[setName].setURL})`);
+				const tankGear = Object.keys(subkeys.gearTank).map(setName => `[${subkeys.gearTank[setName].setName}](${subkeys.gearTank[setName].setURL})`);
+				const restrictions = [
+					`No Mythic Items`, 
+					`No [Blood for Blood](https://eso-hub.com/en/skills/world/vampire/blood-for-blood)`, 
+					`No [Blood Frenzy](https://eso-hub.com/en/skills/world/vampire/blood-frenzy)`]
+				
+				const previousHealerGear = subkeys.previousHealerID? `\nAll ${msgObject.guild.roles.resolve(subkeys.previousHealerID)} sets`: ``;
+				const previousTankGear = subkeys.previousTankID? `\nAll ${msgObject.guild.roles.resolve(subkeys.previousTankID)} sets`: ``;
+
+				const fieldSpace = spacing.repeat( embedWidth / 3 );
+
+				const prerequisiteFields = [
+						{ name: `Damage Dealers`, value: `${ddID}\n${fieldSpace}\n**${dpsMag}**\n**${dpsStam}**\n${restrictions.join(`\n`)}`, inline: true },
+						{ name: `Healers`, value: `${healerID}\n${fieldSpace}\n${healerGear.join(`\n`)}${previousHealerGear}`, inline: true },
+						{ name: `Tanks`, value: `${tankID}\n${fieldSpace}\n${tankGear.join(`\n`)}${previousTankGear}`, inline: true }	
+				];
+
+				// Get Note
+				if(subkeys.note) {
+					prerequisiteFields.push(
+						{name: "Note:", value: subkeys.note, inline: false}
+					)
+				}
+
+				sendEmbed(role.name, "", role.color, prerequisiteFields);
+
+				// Get Additional and Recommended Gear
+				if(subkeys.appendix) {
+					const appendix = prerequisiteAdditionalJSONObject[subkeys.appendix];
+
+					const fieldSpace = spacing.repeat( embedWidth / appendix.fields.length );
+
+					let appendixFields = Object.keys(appendix.fields).map(index => {
+						const gearList = appendix.fields[index].gearList;
+						gearListArray = Object.keys(gearList).map(setName => `[${gearList[setName].setName}](${gearList[setName].setURL})`);
+
+						return {
+							name: appendix.fields[index].name, 
+							value: `\n${fieldSpace}\n${appendix.fields[index].value}${gearListArray.join(`\n`)}`, 
+							inline: appendix.fields[index].inline
+						}
+					 });
+					const role = msgObject.guild.roles.resolve(appendix.roleID);
+					sendEmbed(appendix.title, appendix.description, role.color, appendixFields);
+				}
+			});
+
+		}).catch(error => {
+			console.error(error.message);
+		});
+		
 	},
 };
+
+function createRankEmbed(msgObject, rank) {
+	const ddID = msgObject.guild.roles.resolve(rank.ddID);
+	const healerID = msgObject.guild.roles.resolve(rank.healerID);
+	const tankID = msgObject.guild.roles.resolve(rank.tankID);
+	
+	const requirementsDescription = rank.requirementsDescription? rank.requirementsDescription : `Requirements`;
+	const requirements = Object.keys(rank.requirements).map(index => `${msgObject.guild.roles.resolve(rank.requirements[index].roleID)}`);
+	
+	const rankFields = [
+		{ name: `Damage Dealers`, value: `${ddID}`, inline: true },
+		{ name: `Healers`, value: `${healerID}`, inline: true },
+		{ name: `Tanks`, value: `${tankID}`, inline: true }	,
+		{ name: requirementsDescription, value: `> ${requirements.join(`\n> `)}`, inline: false }
+	];
+	
+	sendEmbed(msgObject, rank.title, "", ddID.color, rankFields);
+}
