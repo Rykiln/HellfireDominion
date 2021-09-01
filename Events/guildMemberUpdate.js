ec54584db29f2be91239b1f6fb2d930729d06bcf
Rykiln <41490808+Rykiln@users.noreply.github.com>
2021-09-01 13:43:17 -0500

    Break Fix: Defined readFile function in guildMemberUpdate.json

--- Events/guildMemberUpdate.js
+++ Events/guildMemberUpdate.js
@@ -1,18 +1,21 @@
+const { MessageEmbed } = require(`discord.js`);
+const { readFile } = require(`fs`);
 module.exports = {
     name: `guildMemberUpdate`,
     once: false,
     execute(oldMember, newMember, client) {
         // Get Guild Information From JSON File
-		const guildHellfireDominion = `./Data/HellfireDominion/guild.json`;
-		readFile(guildHellfireDominion, function (err, data) {
-			if (err) throw err;
-			const dataHellfireDominion = JSON.parse(data);
-            const guildID = dataHellfireDominion.guildID;
+        const guildHellfireDominion = `./Data/HellfireDominion/guild.json`;
+        readFile(guildHellfireDominion, function (err, data) {
+            if (err) throw err;
+            const dataHellfireDominion = JSON.parse(data);
+            const guildID = newMember.guild.id;
             const roles = dataHellfireDominion.roles;
             const rolesSeparator = roles.separator;
-            const { MessageEmbed } = require(`discord.js`)
             if (oldMember.pending === true && newMember.pending === false) {
-                `++ ++ [${newMember.username} has accepted the guild rules.]`;
+                // `++ ++ [${newMember.username} has accepted the guild rules.]`;
+                console.log(`    └ [${newMember.user.tag}] has accepted the guild rules.`);
+
                 // Set Roles To Give When Accepting Membership Screening
                 const rolesIDArray = [
                     roles.default,
@@ -20,17 +23,23 @@ module.exports = {
                     rolesSeparator.clears,
                     rolesSeparator.interests,
                     rolesSeparator.prerequisites,
-                    rolesSeparator.ranks]
+                    rolesSeparator.ranks
+                ]
                 let rolesArray = []
+                console.log(`Assigning Roles:`);
+
                 // Assign Roles To User
                 rolesIDArray.forEach(async (role) => {
                     newMember.roles.add(role);
-                    await rolesArray.push(client.guilds.resolve(guildID).roles.resolve(role).name);
+                    const roleObject = client.guilds.resolve(guildID).roles.resolve(role)
+                    console.log(roleObject.name)
+                    await rolesArray.push(roleObject.name);
                 });
                 const MemberLogChannel = client.guilds.resolve(guildID).channels.resolve(`579451315051167755`);
+                console.log(`Successfully Assigned Roles`);
 
                 // Send Console Log and Embed Notifications
-                console.log(`    └ [${newMember.user.tag}] has accepted the guild rules.`)
+                // console.log(`    └ [${newMember.user.tag}] has accepted the guild rules.`)
                 const embed = new MessageEmbed()
                     .setAuthor(newMember.user.tag.toString(), newMember.user.displayAvatarURL())
                     .setColor(0x00ff00)
